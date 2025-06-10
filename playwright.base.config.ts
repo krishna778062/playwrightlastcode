18f8a9cb4fa7a2f0ec638adf819f53888afe8145
prateek-sqa <prateek.parashar@simpplr.com>
2025-06-10 20:02:16 +0530

    testing out github pages

--- playwright.base.config.ts
+++ playwright.base.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   timeout: TIMEOUTS.VERY_LONG,
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   outputDir: TEST_RESULTS_DIR,
