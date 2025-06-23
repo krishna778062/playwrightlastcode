97d20450ad65c053ba198440dd7f585d01453003
prateek-sqa <prateek.parashar@simpplr.com>
2025-06-23 11:49:34 +0530

    temp change to unblock aditya

--- src/modules/chat/tests/ui-tests/chat-attachment/chat-attachment.spec.ts
+++ src/modules/chat/tests/ui-tests/chat-attachment/chat-attachment.spec.ts
@@ -5,6 +5,8 @@ import { CHAT_TEST_DATA } from '@/src/modules/chat/test-data/chat.test-data';
 import { Roles } from '@core/constants/roles';
 import { MultiUserChatTestHelper } from '@chat/helpers/multiUserChatTestHelper';
 import { ChatHelper } from '@chat/helpers/chatHelper';
+import { getEnvConfig } from '../../../../../core/utils/getEnvConfig';
+import { LoginHelper } from '../../../../../core/helpers/loginHelper';
 
 test.describe(
   'Test chat application with attachment',
@@ -12,51 +14,49 @@ test.describe(
     tag: [TestSuite.CHAT_ATTACHMENT],
   },
   () => {
-    let multiUserChatTest: MultiUserChatTestHelper;
+    // let multiUserChatTest: MultiUserChatTestHelper;
 
-    test.beforeEach(
-      'Setting up the test environment, by creating 1 new user to tenant so to test out attachment in chat',
-      async ({ browser }) => {
-        multiUserChatTest = new MultiUserChatTestHelper();
-        // Step 1: Create all backend test data
-        await multiUserChatTest.setup(browser, {
-          usersByRole: {
-            [Roles.END_USER]: 2,
-          },
-          password: CHAT_TEST_DATA.CREDENTIALS.DEFAULT_PASSWORD,
-        });
-        // Step 2: Create a browser context for ONLY the user needed for this test
-        await multiUserChatTest.createContextsForUsers([0]);
-      }
-    );
+    // test.beforeEach(
+    //   'Setting up the test environment, by creating 1 new user to tenant so to test out attachment in chat',
+    //   async ({ browser }) => {
+    //     multiUserChatTest = new MultiUserChatTestHelper();
+    //     // Step 1: Create all backend test data
+    //     await multiUserChatTest.setup(browser, {
+    //       usersByRole: {
+    //         [Roles.END_USER]: 2,
+    //       },
+    //       password: CHAT_TEST_DATA.CREDENTIALS.DEFAULT_PASSWORD,
+    //     });
+    //     // Step 2: Create a browser context for ONLY the user needed for this test
+    //     await multiUserChatTest.createContextsForUsers([0]);
+    //   }
+    // );
 
-    test.afterEach(async () => {
-      await multiUserChatTest.cleanup();
-    });
+    // test.afterEach(async () => {
+    //   await multiUserChatTest.cleanup();
+    // });
 
-    test(
+    test.only(
       'User is able to send attachment in chat',
       {
         tag: [TestPriority.P0, '@chat-attachment'],
       },
-      async () => {
-        const user1 = multiUserChatTest.getTestData().users[0];
-        const user2 = multiUserChatTest.getTestData().users[1];
-
-        // Login user1  and navigate to chats
-        const user1ChatsPage = await multiUserChatTest.loginAndNavigateToChatsPage(
-          multiUserChatTest.getPageForUser(user1.email),
-          user1.email,
-          CHAT_TEST_DATA.CREDENTIALS.DEFAULT_PASSWORD
-        );
+      async ({ page }) => {
+        const homePage = await LoginHelper.loginWithPassword(page, {
+          email: getEnvConfig().appManagerEmail,
+          password: getEnvConfig().appManagerPassword,
+        });
+        await homePage.verifyThePageIsLoaded();
+        const chatPageUser1 = await homePage.navigateToChatsPage();
+        const user2Name = 'Mya Johnson';
 
         //now open conversation with user 2
-        await ChatHelper.directMessages.openDirectMessageWithUser(user1ChatsPage, user2.fullName, {
-          stepInfo: `User 1 opening direct message with ${user2.fullName}`,
+        await ChatHelper.directMessages.openDirectMessageWithUser(chatPageUser1, user2Name, {
+          stepInfo: `User 1 opening direct message with ${user2Name}`,
         });
 
         //verify that user 1 can send attachment in chat
-        await ChatHelper.common.sendMessage(user1ChatsPage, CHAT_TEST_DATA.MESSAGES.USER1.INITIAL);
+        await ChatHelper.common.sendMessage(chatPageUser1, CHAT_TEST_DATA.MESSAGES.USER1.INITIAL);
       }
     );
   }
