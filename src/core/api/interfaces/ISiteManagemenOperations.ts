60d4747cb9a1af936c855d5d2e1b715b973ff402
prateek-sqa <prateek.parashar@simpplr.com>
2025-07-03 02:00:40 +0530

    update interface for sitemanagement operations

--- src/core/api/interfaces/ISiteManagemenOperations.ts
+++ src/core/api/interfaces/ISiteManagemenOperations.ts
@@ -1,8 +1,7 @@
-import { AddUserResponse } from '@core/types/group.type';
-import { Roles } from '@core/constants/roles';
-import { SiteCreationPayload } from '../services/SiteManagementService';
+import { SiteCreationPayload } from '@core/types/siteManagement.types';
 
 export interface ISiteManagementOperations {
   addNewSite(payload?: Partial<SiteCreationPayload>): Promise<any>;
   deactivateSite(siteId: string): Promise<any>;
+  getCategoryId(category: string): Promise<any>;
 }
