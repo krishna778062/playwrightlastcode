041484d9e41a3b46eaec4ce585d73f9a928d00c9
Nirosha <nirosha.yadla@simpplr.com>
2025-08-07 21:07:30 +0530

    increased timeouts

--- src/modules/global-search/components/intranetFileListComponent.ts
+++ src/modules/global-search/components/intranetFileListComponent.ts
@@ -143,18 +143,9 @@ export class IntranetFileListComponent extends ContentListComponent {
   async clickOnCloseButton() {
     await test.step(`Click on the close button`, async () => {
       await this.page.waitForLoadState('load');
-      await this.verifier.verifyTheElementIsVisible(this.closeButton.first(), { timeout: 50000 });
-
-      const maxRetries = 3;
-      for (let i = 0; i < maxRetries; i++) {
-        await this.clickOnElement(this.closeButton.first());
-        await this.page.waitForTimeout(40_000);
-        if (!(await this.closeButton.first().isVisible())) {
-          return;
-        }
-      }
-      // If the loop finishes, the button is still visible. Fail the test.
-      await this.verifier.verifyTheElementIsNotVisible(this.closeButton.first());
+      await this.verifier.waitUntilElementIsVisible(this.closeButton.first(), { timeout: 50000 });
+      await this.clickOnElement(this.closeButton.first());
+      await this.verifier.verifyTheElementIsNotVisible(this.closeButton.first(), { timeout: 20000 });
     });
   }
 
