7a549e026f6fc6047c32e99db5da3c5423ffed87
prateek-sqa <prateek.parashar@simpplr.com>
2025-08-18 20:49:56 +0530

    fix: tc

--- src/modules/content/fixtures/contentFixture.ts
+++ src/modules/content/fixtures/contentFixture.ts
@@ -1,4 +1,4 @@
-import { Page, test } from '@playwright/test';
+import { BrowserContext, Page, test } from '@playwright/test';
 
 import { AppManagerApiClient } from '@core/api/clients/appManagerApiClient';
 import { ApiClientFactory } from '@core/api/factories/apiClientFactory';
@@ -23,40 +23,58 @@ export const users = {
   },
 };
 
-export const contentTestFixture = test.extend<{
-  appManagerHomePage: NewUxHomePage | OldUxHomePage;
-  appManagersPage: Page;
-  appManagerApiClient: AppManagerApiClient;
-  siteManagementHelper: SiteManagementHelper;
-  feedManagerService: FeedManagerService;
-  loginAs: (userType: UserType) => Promise<void>;
-}>({
+export const contentTestFixture = test.extend<
+  {
+    appManagerContext: BrowserContext;
+    appManagerHomePage: NewUxHomePage | OldUxHomePage;
+    appManagersPage: Page;
+    siteManagementHelper: SiteManagementHelper;
+    feedManagerService: FeedManagerService;
+    loginAs: (userType: UserType) => Promise<void>;
+  },
+  {
+    appManagerApiClient: AppManagerApiClient;
+  }
+>({
+  appManagerApiClient: [
+    async ({}, use, workerInfo) => {
+      console.log(`INFO: Setting up app manager client for worker => `, workerInfo.workerIndex);
+      const appManagerApiClient = await ApiClientFactory.createClient(AppManagerApiClient, {
+        type: 'credentials',
+        credentials: {
+          username: getEnvConfig().appManagerEmail,
+          password: getEnvConfig().appManagerPassword,
+        },
+        baseUrl: getEnvConfig().apiBaseUrl,
+      });
+      await use(appManagerApiClient);
+    },
+    { scope: 'worker' },
+  ],
+  appManagerContext: [
+    async ({ browser }, use, workerInfo) => {
+      const context = await browser.newContext();
+      await use(context);
+      await context?.close();
+    },
+    { scope: 'test' },
+  ],
   appManagerHomePage: [
-    async ({ page }, use, workerInfo) => {
+    async ({ appManagerContext }, use, workerInfo) => {
+      const page = await appManagerContext.newPage();
       const appManagerHomePage = await LoginHelper.loginWithPassword(page, {
         email: getEnvConfig().appManagerEmail,
         password: getEnvConfig().appManagerPassword,
       });
       await appManagerHomePage.verifyThePageIsLoaded();
       await use(appManagerHomePage);
+      await page.close();
     },
     { scope: 'test' },
   ],
   appManagersPage: [
     async ({ appManagerHomePage }, use, workerInfo) => {
       await use(appManagerHomePage.page);
-    },
-    { scope: 'test' },
-  ],
-  appManagerApiClient: [
-    async ({ appManagersPage: appManagerUserPage }, use, workerInfo) => {
-      console.log(`INFO: Setting up app manager client for worker => `, workerInfo.workerIndex);
-      const appManagerApiClient = await ApiClientFactory.createClient(AppManagerApiClient, {
-        type: 'cookies',
-        page: appManagerUserPage,
-        baseUrl: getEnvConfig().apiBaseUrl,
-      });
-      await use(appManagerApiClient);
     },
     { scope: 'test' },
   ],
