77716657158a1821d7a982b000afff02795ec218
LakshmiJoshi1527 <151010600+LakshmiJoshi1527@users.noreply.github.com>
2025-08-21 11:37:08 +0530

    Update pageEndpoints.ts

--- src/core/constants/pageEndpoints.ts
+++ src/core/constants/pageEndpoints.ts
@@ -8,10 +8,13 @@ export const PAGE_ENDPOINTS = {
   //rewards pages
   manageRewardsPage: '/manage/recognition/rewards/overview',
   rewardsOptionsPage: '/manage/recognition/rewards/reward-options',
-  FEATURE_OWNERS: '/manage/access-control/feature-owners',
+  rewardStorePage: '/rewards-store/gift-cards',
+  rewardStoreOrderHistoryPage: '/rewards-store/order-history',
+  //logout
   LOGOUT: '/logout',
 
   //abac pages
+  FEATURE_OWNERS: '/manage/access-control/feature-owners',
   ACCESS_CONTROL_GROUPS_PAGE: '/manage/access-control/groups',
   AUDIENCE_PAGE: '/audiences/org',
   AUDIENCE_RULE_PAGE: '/audiences/org/rules',
