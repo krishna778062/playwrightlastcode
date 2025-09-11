ed68605ad382ccfa0bba129db0035efbda03f6e3
Aman Kumar Mishra <aman.mishra@simpplr.com>
2025-09-11 23:42:49 +0530

    fix: adding @this-one tag for TC execution

--- src/modules/platforms/tests/ui-tests/abac/accessControlGroups/acg.spec.ts
+++ src/modules/platforms/tests/ui-tests/abac/accessControlGroups/acg.spec.ts
@@ -1,11 +1,16 @@
+// @/src imports
 import { AUDIENCE_API_ATTRIBUTES, AUDIENCE_API_OPERATORS } from '@/src/core/constants/createAudienceAPI';
 import { TestSuite } from '@/src/core/constants/testSuite';
 import { TestDataGenerator } from '@/src/core/utils/testDataGenerator';
 import { audienceCreationParams } from '@/src/core/types/audience.type';
+
+// @core imports
 import { POPUP_BUTTONS } from '@core/constants/popupButtons';
 import { TestPriority } from '@core/constants/testPriority';
 import { NewUxHomePage } from '@core/pages/homePage/newUxHomePage';
 import { tagTest } from '@core/utils/testDecorator';
+
+// @platforms imports
 import { ACG_EDIT_ASSETS } from '@platforms/constants/acgEditAssets';
 import { ACG_STATUS } from '@platforms/constants/acgStatus';
 import { platformTestFixture as test } from '@platforms/fixtures/platformFixture';
