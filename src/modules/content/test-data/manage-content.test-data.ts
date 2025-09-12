81daf4f82136259a11ce470b8008b63780827652
rsys-aditya-arora <aditya@radiansys.com>
2025-09-12 10:41:54 +0530

    fix: fix asserstion error of description and added test data generator file

--- src/modules/content/test-data/manage-content.test-data.ts
+++ src/modules/content/test-data/manage-content.test-data.ts
@@ -1,4 +1,4 @@
-import { faker } from '@faker-js/faker/.';
+import { faker } from '@faker-js/faker';
 
 export const MANAGE_CONTENT_TEST_DATA = {
   TITLE: faker.string.alphanumeric(20) + faker.string.numeric(10),
