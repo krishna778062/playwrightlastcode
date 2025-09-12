a5165d42534cf964d5b80a15ce52b92e7c5fc33c
rsys-harsh-mishra <hmishra@radiansys.com>
2025-09-12 15:59:46 +0530

    fix(CONT-36069): validation expired content

---
+++ src/modules/content/components/editPageComponent.ts
@@ -0,0 +1,20 @@
+import { expect, Locator, Page, test } from '@playwright/test';
+
+import { BaseComponent } from '@core/components/baseComponent';
+export class EditPageComponent extends BaseComponent {
+  readonly clickOnCancel: Locator;
+  readonly pageHeading: Locator;
+
+  action: any;
+  constructor(page: Page) {
+    super(page);
+    this.clickOnCancel = page.getByRole('button', { name: 'Cancel' });
+    this.pageHeading = page.getByRole('heading', { name: 'Page' });
+  }
+
+  async clickOnCancelButton(options?: { stepInfo?: string }): Promise<void> {
+    await test.step(options?.stepInfo || 'Clicking on cancel button', async () => {
+      await this.clickOnCancel.click();
+    });
+  }
+}
