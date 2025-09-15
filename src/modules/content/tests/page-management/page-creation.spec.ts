2257f4cea3eee006a529833b282e9ef70ed17f47
rsys-harsh-mishra <hmishra@radiansys.com>
2025-09-15 15:35:43 +0530

    fix(CONT-36069): moved tests from page-creation to page-app-manager file

--- src/modules/content/tests/page-management/page-creation.spec.ts
+++ src/modules/content/tests/page-management/page-creation.spec.ts
@@ -1,31 +1,16 @@
 import { faker } from '@faker-js/faker';
-import { expect, Page } from '@playwright/test';
 
 import { TestPriority } from '@core/constants/testPriority';
 import { TestGroupType } from '@core/constants/testType';
 import { NewUxHomePage } from '@core/pages/homePage/newUxHomePage';
 import { tagTest } from '@core/utils/testDecorator';
 
-import { SiteDashboardPage } from '../../pages/siteDashboardPage';
-
-import { SideNavBarComponent } from '@/src/core/components/sideNavBarComponent';
-import { TopNavBarComponent } from '@/src/core/components/topNavBarComponent';
 import { ContentType } from '@/src/modules/content/constants/contentType';
 import { PageContentType } from '@/src/modules/content/constants/pageContentType';
 import { ContentFeatureTags, ContentSuiteTags } from '@/src/modules/content/constants/testTags';
 import { contentTestFixture as test } from '@/src/modules/content/fixtures/contentFixture';
-import { ApplicationScreenPage } from '@/src/modules/content/pages/applicationscreenPage';
-import { ContentPreviewPage } from '@/src/modules/content/pages/contentPreviewPage';
-import { EditPagePage } from '@/src/modules/content/pages/editPagePage';
-import { GovernanceScreenPage } from '@/src/modules/content/pages/governanceScreenPage';
-import { ManageApplicationPage } from '@/src/modules/content/pages/manageApplicationPage';
-import { ManageContentPage } from '@/src/modules/content/pages/manageContentPage';
-import { ApplicationScreenPage as ManageFeature } from '@/src/modules/content/pages/manageFeaturesPage';
-import { ManageSitePage } from '@/src/modules/content/pages/manageSitePage';
 import { PageCreationPage } from '@/src/modules/content/pages/pageCreationPage';
-import { SiteDetailsPage } from '@/src/modules/content/pages/siteDetailsPage';
 import { CONTENT_TEST_DATA } from '@/src/modules/content/test-data/content.test-data';
-import { SiteDashboard } from '@/src/modules/integrations/pages/siteDashboard';
 
 test.describe(
   ContentSuiteTags.PAGE_CREATION,
@@ -36,36 +21,14 @@ test.describe(
     let pageCreationPage: PageCreationPage;
     let publishedPageId: string;
     let siteIdToPublishPage: string;
-    let pageRef: Page;
     let homePage: NewUxHomePage;
-    let applicationscreen: ApplicationScreenPage;
-    let manageFeaturePage: ManageFeature;
-    let manageApplicationPage: ManageApplicationPage;
-    let governanceScreenPage: GovernanceScreenPage;
-    let manageContentPage: ManageContentPage;
-    let contentPreviewPage: ContentPreviewPage;
-    let manageSitePage: ManageSitePage;
-    let siteDetailsPage: SiteDetailsPage;
-    let siteDashboardPage: SiteDashboardPage;
-    let editPagePage: EditPagePage;
 
     test.beforeEach(async ({ appManagerHomePage, appManagersPage, siteManagementHelper }) => {
       await appManagerHomePage.verifyThePageIsLoaded();
-      // pageRef = pageRef;
 
       homePage = new NewUxHomePage(appManagersPage);
       await homePage.verifyThePageIsLoaded();
       pageCreationPage = new PageCreationPage(appManagersPage, '', siteManagementHelper);
-      applicationscreen = new ApplicationScreenPage(appManagersPage);
-      manageFeaturePage = new ManageFeature(appManagersPage);
-      manageApplicationPage = new ManageApplicationPage(appManagersPage);
-      governanceScreenPage = new GovernanceScreenPage(appManagersPage);
-      manageContentPage = new ManageContentPage(appManagersPage);
-      contentPreviewPage = new ContentPreviewPage(appManagersPage, '', '', '');
-      manageSitePage = new ManageSitePage(appManagersPage, '');
-      siteDetailsPage = new SiteDetailsPage(appManagersPage, '');
-      siteDashboardPage = new SiteDashboardPage(appManagersPage, '', siteManagementHelper);
-      editPagePage = new EditPagePage(appManagersPage);
     });
 
     test.afterEach(async ({ appManagerApiClient }) => {
@@ -116,57 +79,6 @@ test.describe(
         //store the page id
         publishedPageId = pageId;
         siteIdToPublishPage = siteId;
-      }
-    );
-    test(
-      'Verify feed and comment should not be displayed when feed and comments are disabled app level',
-      {
-        tag: [TestPriority.P0, TestGroupType.SMOKE, ContentFeatureTags.VERIFY_COMMENTS_AND_FEEDS],
-      },
-      async () => {
-        tagTest(test.info(), {
-          description: 'Verify feed and comment should not be displayed when feed and comments are disabled app level',
-          zephyrTestId: 'CONT-26613',
-          storyId: 'CONT-26613',
-        });
-        await homePage.actions.navigateToApplication();
-        await applicationscreen.actions.clickOnApplication();
-        await manageApplicationPage.actions.clickOnGovernance();
-        await governanceScreenPage.actions.clickOnTimeline();
-        await governanceScreenPage.actions.clickOnSave();
-        await homePage.actions.clickOnManageFeature();
-        await manageFeaturePage.actions.clickOnContentCard();
-        await manageContentPage.actions.clickOnContent();
-        await contentPreviewPage.actions.checkCommentOption();
-        await homePage.actions.clickOnManageFeature();
-        await manageFeaturePage.actions.clickOnSitesCard();
-        await manageSitePage.actions.clickOnSite();
-        await siteDetailsPage.actions.ViewSite();
-        await siteDashboardPage.actions.verfiyFeedSection();
-        await homePage.actions.clickOnHomeButton();
-        await homePage.actions.clickOnFeedSideMenu();
-        await siteDashboardPage.actions.verfiyFeedSection();
-      }
-    );
-
-    test(
-      'Zeus: Edit the validation Expired Content and Cancel',
-      {
-        tag: [TestPriority.P0, TestGroupType.SMOKE, ContentFeatureTags.VALIDATION_REQUIRED_BAR_STATE],
-      },
-      async () => {
-        tagTest(test.info(), {
-          description: 'Zeus: Edit the validation Expired Content and Cancel',
-          zephyrTestId: 'CONT-36069',
-          storyId: 'CONT-36069',
-        });
-        await homePage.actions.clickOnManageFeature();
-        await manageFeaturePage.actions.clickOnContentCard();
-        await manageContentPage.actions.clickOnViewAllButton();
-        await manageContentPage.actions.verifyingValidationRequiredBarState();
-        await manageContentPage.actions.clickOnEditButton();
-        await editPagePage.actions.clickOnCancel();
-        await manageContentPage.actions.verifyingValidationRequiredBarState();
       }
     );
   }
