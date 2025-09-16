746943a97e7a4af82580c669ad996eb0d79a5ab1
Aniket Srivastava <aniket.srivastava@simpplr.com>
2025-09-16 13:20:21 +0530

    test(ukgSync): added removed file

---
+++ playwright.base.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from '@playwright/test';
+
+import { Modules } from './src/core/constants/modules';
+import { TEST_RESULTS_DIR } from './src/core/constants/paths';
+import { loadEnvVariablesForGivenModule } from './src/core/utils/envLoader';
+
+import { Environments } from '@/src/core/constants/environments';
+import { TIMEOUTS } from '@/src/core/constants/timeouts';
+
+//load all env variables from .env file for given module
+loadEnvVariablesForGivenModule(
+  (process.env.TEST_ENV as Environments) || Environments.QA,
+  process.env.MODULE_NAME || Modules.PLATFORMS
+);
+
+export default defineConfig({
+  forbidOnly: !!process.env.CI,
+  timeout: TIMEOUTS.VERY_LONG,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 2 : 4,
+  use: {
+    actionTimeout: 15_000,
+    navigationTimeout: 30_000,
+    trace: process.env.CI ? 'retain-on-failure' : 'on',
+    screenshot: process.env.CI ? 'only-on-failure' : 'on',
+    headless: process.env.CI ? true : true,
+  },
+  reporter: [
+    ['html', { open: process.env.CI ? 'never' : 'on-failure' }],
+    ['json', { outputFile: `${TEST_RESULTS_DIR}/test-results.json` }],
+  ],
+});
