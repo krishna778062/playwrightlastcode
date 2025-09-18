6a1c23fe5400639d0cf0d0a6aab8ae7f550e642c
Pooja Pathak <pooja.pathak@MACPRO165.local>
2025-09-18 16:08:38 +0530

    refactor: incorporate review comments

--- src/core/utils/getEnvConfig.ts
+++ src/core/utils/getEnvConfig.ts
@@ -5,6 +5,7 @@ export type EnvConfig = {
   userManagerEmail: string;
   frontendBaseUrl: string;
   apiBaseUrl: string;
+  apiBaseUrlPD: string;
   newUxEnabled: boolean;
   //optional env variables (might be required for few modules)
   endUserEmail?: string;
@@ -15,6 +16,7 @@ export const getEnvConfig = (): EnvConfig => ({
   tenantOrgId: getEnvVar('ORG_ID', true)!,
   appManagerEmail: getEnvVar('APP_MANAGER_USERNAME', true)!,
   appManagerPassword: getEnvVar('APP_MANAGER_PASSWORD', true)!,
+
   frontendBaseUrl: getEnvVar('FRONTEND_BASE_URL', true)!,
   apiBaseUrl: getEnvVar('API_BASE_URL', true)!,
   newUxEnabled: getEnvVar('NEW_UX_ENABLED', false) === 'true' || false,
@@ -23,6 +25,8 @@ export const getEnvConfig = (): EnvConfig => ({
   endUserEmail: getEnvVar('END_USER_USERNAME', false)!,
   endUserPassword: getEnvVar('END_USER_PASSWORD', false)!,
   userManagerEmail: getEnvVar('USER_MANAGER_USERNAME', false)!,
+
+  apiBaseUrlPD: getEnvVar('API_BASE_URL_PD', true)!,
 });
 
 /**
