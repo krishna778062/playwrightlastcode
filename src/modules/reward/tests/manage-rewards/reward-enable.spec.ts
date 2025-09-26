ca93bd622e61a49930b8524039fdc84ee44b6d88
Sonu Kumar <sonu.kumar@simpplr.com>
2025-09-26 22:08:07 +0530

    fix: remove the tesps in the code

--- src/modules/reward/tests/manage-rewards/reward-enable.spec.ts
+++ src/modules/reward/tests/manage-rewards/reward-enable.spec.ts
@@ -8,7 +8,7 @@ import { ManageRewardsPage } from '@modules/reward/pages/manage-rewards/manage-r
 
 test.describe('Enable Rewards flow', { tag: [REWARD_SUITE_TAGS.REGRESSION_TEST] }, () => {
   test.only(
-    '[RC-3014] Validate Enable rewards flow',
+    'Validate Enable rewards flow',
     {
       tag: [REWARD_SUITE_TAGS.REGRESSION_TEST, TestPriority.P0, TestGroupType.SMOKE],
     },
@@ -19,46 +19,35 @@ test.describe('Enable Rewards flow', { tag: [REWARD_SUITE_TAGS.REGRESSION_TEST] 
         storyId: 'RC-3014',
       });
       const manageRewardsPage = new ManageRewardsPage(appManagerPage);
+      await manageRewardsPage.loadPage();
+      await manageRewardsPage.verifyThePageIsLoaded();
 
-      await test.step('Login to the application and navigate to Rewards overview', async () => {
-        await manageRewardsPage.loadPage();
-        await manageRewardsPage.verifyThePageIsLoaded();
-      });
+      const isEnableRewardButtonDisplayed = await manageRewardsPage.verifier.isTheElementVisible(
+        manageRewardsPage.disableRewardLink,
+        { timeout: 10000 }
+      );
 
-      await test.step('Check if the Rewards is Disabled and Enable it', async () => {
-        const isEnableRewardButtonDisplayed = await manageRewardsPage.verifier.isTheElementVisible(
-          manageRewardsPage.disableRewardLink,
-          { timeout: 10000 }
-        );
+      if (!isEnableRewardButtonDisplayed) {
+        // Scenario 1: Rewards are already turn off → Enable directly
+        await manageRewardsPage.clickOnElement(manageRewardsPage.enableRewardsButton, {
+          stepInfo: 'Clicking on enable rewards button',
+        });
+        await manageRewardsPage.verifyToastMessage('Rewards enabled');
+        await manageRewardsPage.verifier.verifyElementHasText(manageRewardsPage.rewardsTabHeading, 'Rewards overview');
+      } else {
+        await manageRewardsPage.verifier.verifyTheElementIsVisible(manageRewardsPage.disableRewardLink);
+        await manageRewardsPage.disableTheRewards();
+        await manageRewardsPage.verifier.waitUntilElementIsVisible(manageRewardsPage.rewardsTabHeading);
 
-        if (!isEnableRewardButtonDisplayed) {
-          // Scenario 1: Rewards are already turn off → Enable directly
-          await manageRewardsPage.clickOnElement(manageRewardsPage.enableRewardsButton, {
-            stepInfo: 'Clicking on enable rewards button',
-          });
-          await manageRewardsPage.verifyToastMessage('Rewards enabled');
-          await manageRewardsPage.verifier.verifyElementHasText(
-            manageRewardsPage.rewardsTabHeading,
-            'Rewards overview'
-          );
-        } else {
-          await manageRewardsPage.verifier.verifyTheElementIsVisible(manageRewardsPage.disableRewardLink);
-          await manageRewardsPage.disableTheRewards();
-          await manageRewardsPage.verifier.waitUntilElementIsVisible(manageRewardsPage.rewardsTabHeading);
-
-          // Enable again
-          await manageRewardsPage.page.reload();
-          await manageRewardsPage.verifier.verifyTheElementIsVisible(manageRewardsPage.enableRewardsButton);
-          await manageRewardsPage.clickOnElement(manageRewardsPage.enableRewardsButton, {
-            stepInfo: 'Clicking on enable rewards button after reload',
-          });
-          await manageRewardsPage.verifyToastMessage('Rewards enabled');
-          await manageRewardsPage.verifier.verifyElementHasText(
-            manageRewardsPage.rewardsTabHeading,
-            'Rewards overview'
-          );
-        }
-      });
+        // Enable again
+        await manageRewardsPage.page.reload();
+        await manageRewardsPage.verifier.verifyTheElementIsVisible(manageRewardsPage.enableRewardsButton);
+        await manageRewardsPage.clickOnElement(manageRewardsPage.enableRewardsButton, {
+          stepInfo: 'Clicking on enable rewards button after reload',
+        });
+        await manageRewardsPage.verifyToastMessage('Rewards enabled');
+        await manageRewardsPage.verifier.verifyElementHasText(manageRewardsPage.rewardsTabHeading, 'Rewards overview');
+      }
     }
   );
 });
