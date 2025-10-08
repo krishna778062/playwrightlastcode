7333983eb468fba91c448c5e2a56cc6581eb0720
kanika.arora <kanika.arora@simpplr.com>
2025-10-08 18:43:34 +0530

    test(docebo-app-tiles): Added multi user test cases for Docebo App Tiles

---
+++ src/modules/integrations/tests/app-tiles/docebo.multiuser.spec.ts
@@ -0,0 +1,93 @@
+import { faker } from '@faker-js/faker';
+import { UI_ACTIONS } from '@integrations-constants/common';
+import { MESSAGES } from '@integrations-constants/messageRepo';
+import { IntegrationsSuiteTags } from '@integrations-constants/testTags';
+import { multiUserTileFixture } from '@integrations-fixtures/multiUserTileFixture';
+import { test } from '@playwright/test';
+
+import { TestPriority } from '@core/constants/testPriority';
+import { TestGroupType } from '@core/constants/testType';
+import { tagTest } from '@core/utils/testDecorator';
+
+import { waitUntilTilePresentInApi } from '@/src/modules/integrations/api/helpers/tileApiHelpers';
+import { HomeDashboard } from '@/src/modules/integrations/pages/homeDashboard';
+import { SiteDashboard } from '@/src/modules/integrations/pages/siteDashboard';
+
+test.describe(
+  'Docebo App Tiles Multi-user Tests',
+  {
+    tag: [IntegrationsSuiteTags.DOCEBO, IntegrationsSuiteTags.ABSOLUTE],
+  },
+  () => {
+    const AppName = 'Docebo';
+    const tileName = 'Display learning courses';
+    let createdTileTitle: string | undefined = undefined;
+
+    multiUserTileFixture.afterEach(async ({ adminPage, tileManagementHelper }) => {
+      if (createdTileTitle) {
+        const homeDashboard = new HomeDashboard(adminPage, tileManagementHelper);
+        await tileManagementHelper.removeIntegrationAppTile(createdTileTitle);
+        await homeDashboard.verifyTileRemoved(createdTileTitle);
+        createdTileTitle = undefined;
+      }
+    });
+
+    multiUserTileFixture(
+      'Verify the "Docebo App" tile is visible to end users after it has been added by the App Manager',
+      {
+        tag: [TestPriority.P1, TestGroupType.SANITY],
+      },
+      async ({ adminPage, endUserPage, tileManagementHelper }) => {
+        tagTest(multiUserTileFixture.info(), {
+          zephyrTestId: 'INT-28268',
+          storyId: 'INT-24422',
+        });
+
+        //Generate a random tile title
+        createdTileTitle = `Docebo report ${faker.string.alphanumeric({ length: 6 })}`;
+
+        // Add tile, verify by both users, then remove
+        const adminHomeDashboard = new HomeDashboard(adminPage, tileManagementHelper);
+        await adminHomeDashboard.addTile(createdTileTitle, AppName, tileName, UI_ACTIONS.ADD_TO_HOME);
+        await adminHomeDashboard.verifyToastMessage(MESSAGES.ADD_TILE_SUCCESS_MESSAGE);
+        await adminHomeDashboard.isTilePresent(createdTileTitle);
+        const endUserHomeDashboard = new HomeDashboard(endUserPage, tileManagementHelper);
+        await waitUntilTilePresentInApi(endUserPage, createdTileTitle);
+        await endUserHomeDashboard.reloadAndVerifyTilePresent(createdTileTitle);
+      }
+    );
+
+    multiUserTileFixture(
+      'Verify the "Docebo App" tile is visible to end users on Site Dashboard',
+      {
+        tag: [TestPriority.P1, TestGroupType.SANITY],
+      },
+      async ({ adminPage, endUserPage, siteManagementHelper, appManagerApiClient, tileManagementHelper }) => {
+        tagTest(multiUserTileFixture.info(), {
+          zephyrTestId: 'INT-28270',
+          storyId: 'INT-24422',
+        });
+
+        //Generate a random tile title
+        createdTileTitle = `Docebo report${faker.string.alphanumeric({ length: 6 })}`;
+        const endUserSiteDashboard = new SiteDashboard(endUserPage);
+        const siteDashboard = new SiteDashboard(adminPage, tileManagementHelper);
+
+        // Create site and navigate
+        const category = await appManagerApiClient.getSiteManagementService().getCategoryId('Uncategorized');
+        const createdSite = await siteManagementHelper.createPublicSite({ category });
+        await siteDashboard.navigateToSite(createdSite.siteId);
+
+        // Add tile, verify by both users, then remove
+        await siteDashboard.addTile(createdTileTitle, AppName, tileName, UI_ACTIONS.ADD_TO_SITE);
+        await siteDashboard.verifyToastMessage(MESSAGES.ADD_TILE_SUCCESS_MESSAGE);
+        await siteDashboard.isTilePresent(createdTileTitle);
+        await endUserSiteDashboard.navigateToSite(createdSite.siteId);
+        await waitUntilTilePresentInApi(endUserPage, createdTileTitle);
+        await endUserSiteDashboard.isTilePresent(createdTileTitle);
+        await siteDashboard.removeTile(createdTileTitle, MESSAGES.REMOVED_TILE_SUCCESS_MESSAGE);
+        createdTileTitle = undefined;
+      }
+    );
+  }
+);
