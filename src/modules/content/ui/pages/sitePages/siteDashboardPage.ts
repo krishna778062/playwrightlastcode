487e18da946428d432514fb320ac0b5b2e5dab4f
SonaliGupta0608 <sonali.gupta@simpplr.com>
2025-10-16 12:10:49 +0530

    chore: added site question

--- src/modules/content/ui/pages/sitePages/siteDashboardPage.ts
+++ src/modules/content/ui/pages/sitePages/siteDashboardPage.ts
@@ -4,7 +4,11 @@ import { ListFeedComponent } from '@content-components/listFeedComponent';
 import { SiteDashboardComponent } from '@content-components/siteDashboardComponent';
 import { expect, Locator, Page, test } from '@playwright/test';
 
+import { AddTileComponent } from '@content/ui/components/addTileComponent';
 import { BaseSitePage } from '@content/ui/pages/sitePages/baseSite';
+
+import { CreateFeedPostComponent } from '../../components/createFeedPostComponent';
+import { CreateQuestionComponent, QuestionOptions, QuestionResult } from '../../components/createQuestionComponent';
 
 import { PAGE_ENDPOINTS } from '@/src/core/constants/pageEndpoints';
 
@@ -18,6 +22,15 @@ export interface ISiteDashboardActions {
   enterSearchCarouselInput: (text: string) => Promise<void>;
   selectCarouselItem: (text: string) => Promise<void>;
   clickDoneButton: () => Promise<void>;
+  clickOnSocialCampaignTile: () => Promise<void>;
+  clickOnCustomSCTile: () => Promise<void>;
+  enterTileTitle: (tileTitle: string) => Promise<void>;
+  setCustomSCTitle: (title: string) => Promise<void>;
+  clickAddToHomeButton: () => Promise<string>;
+  clickShareThoughtsButton: () => Promise<void>;
+  clickQuestionButton: () => Promise<void>;
+  createAndPostQuestion: (options: QuestionOptions) => Promise<QuestionResult>;
+  editQuestion: (questionTitle: string, newTitle: string) => Promise<void>;
 }
 
 export interface ISiteDashboardAssertions {
@@ -30,6 +43,10 @@ export interface ISiteDashboardAssertions {
   verifySocalCampaignInCarouselItem: (text: string) => Promise<void>;
   verifySocalCampaignIsNotInCarouselItem: (text: string) => Promise<void>;
   verifySocialCampaignShareButtonIsNotVisible: (description: string) => Promise<void>;
+  verifyTileIsDisplayed: (tileTitle: string) => Promise<void>;
+  verifySocialCampaignNameInTheDisplayed: (socialCampaignName: string) => Promise<void>;
+  verifySocialCampaignNameNotDisplayed: (socialCampaignName: string) => Promise<void>;
+  verifyQuestionCreatedSuccessfully: (questionTitle: string) => Promise<void>;
 }
 
 export class SiteDashboardPage extends BaseSitePage implements ISiteDashboardAssertions {
@@ -40,12 +57,19 @@ export class SiteDashboardPage extends BaseSitePage implements ISiteDashboardAss
   readonly feedLink: Locator;
   readonly editDashboardButton = this.page.locator('div[data-title="Edit dashboard"]');
   readonly carouselItemText = (text: string) => this.page.locator('div').filter({ hasText: text });
+  readonly tileListComponent = (tileTitle: string) => this.page.getByRole('heading', { name: tileTitle });
+  readonly socialCampaignNameInTileList = (socialCampaignName: string) =>
+    this.page.getByRole('button', { name: socialCampaignName }).first();
+  readonly shareThoughtsButton: Locator;
 
   // Components
   readonly listFeedComponent: ListFeedComponent;
   readonly siteDashboardComponent: SiteDashboardComponent;
   private carouselComponent: CarouselComponent;
   private editbarComponent: EditBarComponent;
+  private addTileComponent: AddTileComponent;
+  private createQuestionComponent: CreateQuestionComponent;
+  private createFeedPostComponent: CreateFeedPostComponent;
   // Actions
   get actions(): ISiteDashboardActions {
     return this;
@@ -57,10 +81,14 @@ export class SiteDashboardPage extends BaseSitePage implements ISiteDashboardAss
     this.listFeedComponent = new ListFeedComponent(page);
     this.carouselComponent = new CarouselComponent(page);
     this.editbarComponent = new EditBarComponent(page);
+    this.addTileComponent = new AddTileComponent(page);
+    this.createFeedPostComponent = new CreateFeedPostComponent(page);
+    this.createQuestionComponent = new CreateQuestionComponent(page);
     this.feedLink = this.page.locator('a:has-text("eed")');
     this.categoryLink = (categoryName: string) => this.page.getByRole('link', { name: categoryName });
     this.categoryHeading = (categoryName: string) => this.page.getByRole('heading', { name: categoryName });
     this.siteLink = (siteName: string) => this.page.getByRole('link', { name: siteName });
+    this.shareThoughtsButton = this.page.locator('span', { hasText: 'Share your thought' });
   }
   /**
    * Verifies that site was created successfully by checking if site link is visible
@@ -180,5 +208,77 @@ export class SiteDashboardPage extends BaseSitePage implements ISiteDashboardAss
 
   async selectCarouselItem(text: string): Promise<void> {
     return this.carouselComponent.selectCarouselItem(text);
+  }
+
+  async clickOnSocialCampaignTile(): Promise<void> {
+    return this.addTileComponent.clickSocialCampaignsButton();
+  }
+
+  async clickOnCustomSCTile(): Promise<void> {
+    return this.addTileComponent.clickCustomTab();
+  }
+
+  async enterTileTitle(tileTitle: string): Promise<void> {
+    return this.addTileComponent.setTileTitle(tileTitle);
+  }
+
+  async setCustomSCTitle(title: string): Promise<void> {
+    return this.addTileComponent.setCustomSCTitle(title);
+  }
+
+  async clickAddToHomeButton(): Promise<string> {
+    return this.addTileComponent.clickAddToHomeButton();
+  }
+
+  async verifyTileIsDisplayed(tileTitle: string): Promise<void> {
+    await test.step('Verifying tile is displayed', async () => {
+      await this.verifier.verifyTheElementIsVisible(this.tileListComponent(tileTitle), {
+        timeout: 20000,
+        assertionMessage: `Tile title '${tileTitle}' should be displayed`,
+      });
+    });
+  }
+
+  async verifySocialCampaignNameInTheDisplayed(socialCampaignName: string): Promise<void> {
+    await test.step('Verifying social campaign name is displayed in the displayed', async () => {
+      await this.verifier.verifyTheElementIsVisible(this.socialCampaignNameInTileList(socialCampaignName), {
+        timeout: 20000,
+        assertionMessage: `Social campaign name '${socialCampaignName}' should be displayed`,
+      });
+    });
+  }
+
+  async verifySocialCampaignNameNotDisplayed(socialCampaignName: string): Promise<void> {
+    await test.step('Verifying social campaign name is displayed in the displayed', async () => {
+      await this.verifier.verifyTheElementIsNotVisible(this.socialCampaignNameInTileList(socialCampaignName), {
+        timeout: 20000,
+        assertionMessage: `Social campaign name '${socialCampaignName}' should be displayed`,
+      });
+    });
+  }
+
+  /**
+   * Clicks the share thoughts button to open post editor
+   */
+  async clickShareThoughtsButton(): Promise<void> {
+    await test.step('Click on Share your thoughts button', async () => {
+      await this.clickOnElement(this.shareThoughtsButton);
+    });
+  }
+
+  async clickQuestionButton(): Promise<void> {
+    await this.createFeedPostComponent.clickQuestionButton();
+  }
+
+  async createAndPostQuestion(options: QuestionOptions): Promise<QuestionResult> {
+    return this.createQuestionComponent.createAndPostQuestion(options);
+  }
+
+  async editQuestion(questionTitle: string, newTitle: string): Promise<void> {
+    await this.createQuestionComponent.editQuestion(questionTitle, newTitle);
+  }
+
+  async verifyQuestionCreatedSuccessfully(questionTitle: string): Promise<void> {
+    await this.createQuestionComponent.verifyQuestionCreatedSuccessfully(questionTitle);
   }
 }
