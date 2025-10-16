78a85344c0370f64bd191812f6735584468160af
kanika.arora <kanika.arora@simpplr.com>
2025-10-16 13:01:23 +0530

    fix(docebo.spec): correct spacing in test description for "Show more" visibility

--- src/modules/integrations/tests/app-tiles/docebo.spec.ts
+++ src/modules/integrations/tests/app-tiles/docebo.spec.ts
@@ -374,7 +374,7 @@ test.describe(
       }
     );
     test(
-      'verify Show more is visible after 4 courses for pending learning courses from Docebo on a tile on Site Dashboard',
+      'verify Show more is visible after 4 courses for pending learning courses from Docebo on a tile on  Site Dashboard',
       {
         tag: [TestPriority.P1, TestGroupType.SANITY, TEST_TAGS.SHOW_MORE],
       },
