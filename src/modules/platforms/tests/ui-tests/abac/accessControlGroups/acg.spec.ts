1ad1c9ae5a1003aac4f1f5174c45c8b658b3babb
Aman Kumar Mishra <aman.mishra@simpplr.com>
2025-11-07 15:54:20 +0530

    Fix: introducing @this-one tag for TC execution

--- src/modules/platforms/tests/ui-tests/abac/accessControlGroups/acg.spec.ts
+++ src/modules/platforms/tests/ui-tests/abac/accessControlGroups/acg.spec.ts
@@ -482,7 +482,7 @@ test.describe(
     test(
       'verify that duplicate acg error is displayed on editing ACG to match anothers features and target audiences',
       {
-        tag: [TestPriority.P0, `@ABAC`, `@acg`],
+        tag: [TestPriority.P0, `@ABAC`, `@acg`, `@this-one`],
       },
       async ({ appManagerFixture }) => {
         tagTest(test.info(), {
