0467c6040fb61f1e9751f72254aabe184d70dbc0
PrashanttBG <baghelprashant180@gmail.com>
2025-11-12 19:51:57 +0530

    fix(CONT-26712): add tag in test

--- src/modules/content/tests/content-common/feed/feed-share-comment-to-site-feed.spec.ts
+++ src/modules/content/tests/content-common/feed/feed-share-comment-to-site-feed.spec.ts
@@ -132,7 +132,7 @@ test.describe(
     test(
       'verify that a user can share a Content Feed post comment with a message to both a Private Site and an Unlisted Site using the "Post in SITE FEED" option',
       {
-        tag: [TestPriority.P0, TestGroupType.SMOKE, TestGroupType.REGRESSION],
+        tag: [TestPriority.P0, TestGroupType.SMOKE, TestGroupType.REGRESSION, '@CONT-26712'],
       },
       async ({ siteManagerFixture, standardUserFixture }) => {
         tagTest(test.info(), {
