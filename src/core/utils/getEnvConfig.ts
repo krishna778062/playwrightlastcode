a2b96e64b47ab8cecf1896941f187aafecfefee8
SonaliGupta0608 <Sonali.gupta@simpplr.com>
2025-11-13 12:37:51 +0530

    fix: revert changes

--- src/core/utils/getEnvConfig.ts
+++ src/core/utils/getEnvConfig.ts
@@ -19,33 +19,31 @@ export type EnvConfig = {
   promotionManagerPassword?: string;
 };
 
-export const getEnvConfig = (): EnvConfig => {
-  return {
-    tenantOrgId: getEnvVar('ORG_ID', false),
-    appManagerEmail: getEnvVar('APP_MANAGER_USERNAME', true)!,
-    userManagerEmail: getEnvVar('USER_MANAGER_USERNAME', false)!,
-    appManagerPassword: getEnvVar('APP_MANAGER_PASSWORD', true)!,
+export const getEnvConfig = (): EnvConfig => ({
+  tenantOrgId: getEnvVar('ORG_ID', false),
+  appManagerEmail: getEnvVar('APP_MANAGER_USERNAME', true)!,
+  userManagerEmail: getEnvVar('USER_MANAGER_USERNAME', false)!,
+  appManagerPassword: getEnvVar('APP_MANAGER_PASSWORD', true)!,
 
-    frontendBaseUrl: getEnvVar('FRONTEND_BASE_URL', true)!,
-    apiBaseUrl: getEnvVar('API_BASE_URL', true)!,
-    apiBaseUrlPD: getEnvVar('API_BASE_URL_PD', false),
-    newUxEnabled: getEnvVar('NEW_UX_ENABLED', false) === 'true' || false,
-    tenantUserRoleId: getEnvVar('TENANT_USER_ROLE_ID', false),
+  frontendBaseUrl: getEnvVar('FRONTEND_BASE_URL', true)!,
+  apiBaseUrl: getEnvVar('API_BASE_URL', true)!,
+  apiBaseUrlPD: getEnvVar('API_BASE_URL_PD', false),
+  newUxEnabled: getEnvVar('NEW_UX_ENABLED', false) === 'true' || false,
+  tenantUserRoleId: getEnvVar('TENANT_USER_ROLE_ID', false),
 
-    //optional env variables (might be required for few modules)
-    endUserEmail: getEnvVar('END_USER_USERNAME', false)!,
-    endUserPassword: getEnvVar('END_USER_PASSWORD', false)!,
+  //optional env variables (might be required for few modules)
+  endUserEmail: getEnvVar('END_USER_USERNAME', false)!,
+  endUserPassword: getEnvVar('END_USER_PASSWORD', false)!,
 
-    siteManagerEmail: getEnvVar('SITE_MANAGER_USERNAME', false)!,
-    siteManagerPassword: getEnvVar('SITE_MANAGER_PASSWORD', false)!,
+  siteManagerEmail: getEnvVar('SITE_MANAGER_USERNAME', false)!,
+  siteManagerPassword: getEnvVar('SITE_MANAGER_PASSWORD', false)!,
 
-    socialCampaignManagerEmail: getEnvVar('SOCIAL_CAMPAIGN_MANAGER', false)!,
-    socialCampaignManagerPassword: getEnvVar('SOCIAL_CAMPAIGN_PASSWORD', false)!,
+  socialCampaignManagerEmail: getEnvVar('SOCIAL_CAMPAIGN_MANAGER', false)!,
+  socialCampaignManagerPassword: getEnvVar('SOCIAL_CAMPAIGN_PASSWORD', false)!,
 
-    promotionManagerEmail: getEnvVar('PROMOTION_MANAGER_USERNAME', false)!,
-    promotionManagerPassword: getEnvVar('PROMOTION_MANAGER_PASSWORD', false)!,
-  };
-};
+  promotionManagerEmail: getEnvVar('PROMOTION_MANAGER_USERNAME', false)!,
+  promotionManagerPassword: getEnvVar('PROMOTION_MANAGER_PASSWORD', false)!,
+});
 
 /**
  * @param key - The environment variable key to retrieve
