26c098caf0d0edbd95b3a3e8edac91468eab2f45
PrashanttBG <baghelprashant180@gmail.com>
2025-11-18 16:10:34 +0530

    fix(CONT-30148): correct siteId error

--- src/modules/content/tests/content-common/feed/feed-reply.spec.ts
+++ src/modules/content/tests/content-common/feed/feed-reply.spec.ts
@@ -46,7 +46,7 @@ async function getPrerequisiteData(
   // Create site only once, even if both createSite and createPage are true
   if (testData.feedType === 'Site Feed') {
     const siteResult = await helpers.siteManagementHelper.getSiteByAccessType('public');
-    resources.siteId = siteResult;
+    resources.siteId = siteResult.siteId;
   }
 
   if (testData.feedType === 'Content Feed') {
