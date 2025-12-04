65b62cc9e19a90877312e29b43cef96212f6ac1f
krishnasingh7780 <krishna.singh@simpplr.com>
2025-12-04 11:18:39 +0530

    fix: error message is changed for test email

--- src/modules/alert-notification/constants/messageRepo.ts
+++ src/modules/alert-notification/constants/messageRepo.ts
@@ -1,8 +1,12 @@
 export const ALERT_NOTIFICATION_MESSAGES = {
   CUSTOMIZATION_SAVED: 'Customization saved',
   CUSTOMIZATION_DELETED: 'Customization deleted',
-  TEST_EMAIL_SENT_SUCCESS: 'Test email sent successfully',
-  CUSTOM_EMAIL_SUBJECT_TEST_SENT: 'Custom Email subject: Test email sent successfully',
+  CUSTOM_EMAIL_SUBJECT_TEST_SENT: 'Test email sent',
   INVALID_EMAIL_ERROR: 'Please enter a valid email address',
   INVALID_SUBJECT_LINE_ERROR: 'Custom subject is required',
+  MOBILE_PROMOTION_EMAIL_SMS_SENT: 'App download link sent to ',
+  USER_NOT_FOUND: 'User not found',
+  INVALID_PHONE_NUMBER: 'Invalid phone number',
+  INVALID_PHONE_NUMBER_INPUTFIELD_ERROR: 'Please enter a valid phone number',
+  MOBILE_PROMOTION_SENT_SUCCESSFULLY: 'App download link sent to ',
 } as const;
