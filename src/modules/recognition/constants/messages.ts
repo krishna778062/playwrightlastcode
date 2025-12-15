87baa22ad7824f302667314fbebe37ec5bc9bffd
aishma.gupta <aishma.gupta@simpplr.com>
2025-12-16 00:29:15 +0530

    test(rc-6969): added 3 tests

--- src/modules/recognition/constants/messages.ts
+++ src/modules/recognition/constants/messages.ts
@@ -2,4 +2,11 @@ export const MESSAGES = {
   FEATURE_NOT_AVAILABLE: 'This feature is not currently enabled. Please contact your account manager to learn more.',
   NEW_AWARD_CREATED: 'New award created',
   AWARD_DELETED: 'Award deleted',
-} as const;
+};
+
+export const AWARD_CREATION_MESSAGES = {
+  DELEGATE_SELECTION_BY: 'Select when the award delegate needs to select the winner(s) by.',
+  AWARD_ISSUED_ON_FIRST_DAY: 'Awards will be issued on the first day of the following',
+};
+
+export default MESSAGES;
