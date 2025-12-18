3e6ec036c2cfe5d0bf8c91a6b51582d19c7a3cb1
Aman Kumar Mishra <aman.mishra@simpplr.com>
2025-12-18 11:49:36 +0530

    fix: fixeds failure TCs from report

--- src/modules/platforms/tests/ui-tests/abac/featureOwners/featureOwners.spec.ts
+++ src/modules/platforms/tests/ui-tests/abac/featureOwners/featureOwners.spec.ts
@@ -268,7 +268,7 @@ test.describe(
       test(
         `Verify that when primary role of a user is changed from manage users page then the privileges should be lost or gained accordingly for ${feature} feature`,
         {
-          tag: [TestPriority.P1, `@ABAC`, `@featureOwners`],
+          tag: [TestPriority.P1, `@ABAC`, `@featureOwners`, `@this-one`],
         },
         async ({ appManagerFixture, browser }) => {
           tagTest(test.info(), {
