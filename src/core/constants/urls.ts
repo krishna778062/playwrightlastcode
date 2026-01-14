76549eeff1bebf51a3ffb5bfd177e939ddda8df2
Arvind Singh <arvind.singh@simpplr.com>
2026-01-14 19:05:23 +0530

    chore: update urls

--- src/core/constants/urls.ts
+++ src/core/constants/urls.ts
@@ -8,8 +8,13 @@ export const BACKEND_URLS = {
   'qa.simpplr.xyz': 'https://api-be.qa.simpplr.xyz',
   'perf.simpplr.xyz': 'https://api-be.perf.simpplr.xyz',
   // UAT .simpplr.com domains - backend is on .xyz (SSL cert is only for *.simpplr.xyz)
-  'uat-eu.simpplr.com': 'https://api-be.uat-eu.simpplr.xyz',
   'uat.simpplr.com': 'https://api-be.uat.simpplr.xyz',
+  'uat-eu.simpplr.com': 'https://api-be.uat-eu.simpplr.xyz',
+  'uat-ca.simpplr.com': 'https://api-be.uat-ca.simpplr.com',
+  'uat-au.simpplr.com': 'https://api-be.uat-au.simpplr.com',
   // Production .simpplr.com domains - has proper SSL certs
   'app.simpplr.com': 'https://api-be.app.simpplr.com',
+  'eu.simpplr.com': 'https://api-be.eu.simpplr.com',
+  'ca.simpplr.com': 'https://api-be.ca.simpplr.com',
+  'au.simpplr.com': 'https://api-be.au.simpplr.com',
 } as const;
