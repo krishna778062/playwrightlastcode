e402d55e7342200e4055c4f6d4c189eae589e8da
adil.shamim <adil.shamim@simpplr.com>
2026-01-20 19:47:32 +0530

    fix(SocialInteraction): add known failure

--- src/modules/data-engineering/tests/ui-tests/social-interaction-dashboard/static-period-filter-impact.spec.ts
+++ src/modules/data-engineering/tests/ui-tests/social-interaction-dashboard/static-period-filter-impact.spec.ts
@@ -220,12 +220,15 @@ test.describe(
       `verify Least engaged by Department tabular data validation for period as ${periodFilterTimeRange}`,
       {
         tag: [TestPriority.P0, TestGroupType.REGRESSION, TestCaseType.TABULAR_METRIC, '@least-engaged-by-department'],
+        annotation: { type: 'known_failure', description: 'DE-27645' },
       },
       async () => {
         tagTest(test.info(), {
           description: 'To verify the answer of Least engaged by Department in Social Interaction dashboard',
           zephyrTestId: 'DE-27866',
           storyId: 'DE-25760',
+          isKnownFailure: true,
+          bugTicket: 'DE-27645',
         });
 
         const { socialInteractionQueryHelper } = testEnvironment;
@@ -278,12 +281,15 @@ test.describe(
       `verify Most engaged by Department tabular data validation for period as ${periodFilterTimeRange}`,
       {
         tag: [TestPriority.P0, TestGroupType.REGRESSION, TestCaseType.TABULAR_METRIC, '@most-engaged-by-department'],
+        annotation: { type: 'known_failure', description: 'DE-27645' },
       },
       async () => {
         tagTest(test.info(), {
           description: 'To verify the answer of Most engaged by Department in Social Interaction dashboard',
           zephyrTestId: 'DE-27863',
           storyId: 'DE-25757',
+          isKnownFailure: true,
+          bugTicket: 'DE-27645',
         });
 
         const { socialInteractionQueryHelper } = testEnvironment;
@@ -309,6 +315,7 @@ test.describe(
           TestCaseType.CSV_VALIDATION,
           '@most-engaged-by-department-csv',
         ],
+        annotation: { type: 'known_failure', description: 'DE-27645' },
       },
       async () => {
         tagTest(test.info(), {
@@ -316,6 +323,8 @@ test.describe(
             'To verify CSV download and validation for Most engaged by Department in Social Interaction dashboard',
           zephyrTestId: 'DE-27872',
           storyId: 'DE-25767',
+          isKnownFailure: true,
+          bugTicket: 'DE-27645',
         });
 
         const { socialInteractionQueryHelper } = testEnvironment;
