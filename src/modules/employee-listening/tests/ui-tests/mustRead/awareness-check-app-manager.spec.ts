f8d56ce567488f1f49e1b1f8f84acf546894e9f1
ravina-Simpplr <ravina.habbu@simpplr.com>
2026-01-29 13:50:07 +0530

    fix: uncommented code

--- src/modules/employee-listening/tests/ui-tests/mustRead/awareness-check-app-manager.spec.ts
+++ src/modules/employee-listening/tests/ui-tests/mustRead/awareness-check-app-manager.spec.ts
@@ -82,9 +82,9 @@ test.describe('must Read and Awareness Check Content Functionality', () => {
 
       await awarenessCheckPage.clickOnMakeMustReadButton();
 
-      // await awarenessCheckPage.verifyAwarenessCheckQuestionIsCreated(
-      //   EMPLOYEE_LISTENING_TEST_DATA.AWARENESS_CHECK.QUESTIONS.SINGLE.question
-      // );
+      await awarenessCheckPage.verifyAwarenessCheckQuestionIsCreated(
+        EMPLOYEE_LISTENING_TEST_DATA.AWARENESS_CHECK.QUESTIONS.SINGLE.question
+      );
     }
   );
 
@@ -115,9 +115,9 @@ test.describe('must Read and Awareness Check Content Functionality', () => {
 
       await awarenessCheckPage.clickOnMakeMustReadButton();
 
-      // await awarenessCheckPage.verifyAwarenessCheckQuestionIsCreated(
-      //   EMPLOYEE_LISTENING_TEST_DATA.AWARENESS_CHECK.QUESTIONS.MULTIPLE[0].question
-      // );
+      await awarenessCheckPage.verifyAwarenessCheckQuestionIsCreated(
+        EMPLOYEE_LISTENING_TEST_DATA.AWARENESS_CHECK.QUESTIONS.MULTIPLE[0].question
+      );
     }
   );
 
@@ -165,9 +165,9 @@ test.describe('must Read and Awareness Check Content Functionality', () => {
 
       await awarenessCheckPage.updateAwarenessCheckButton.click();
 
-      // await awarenessCheckPage.verifyAwarenessCheckQuestionIsCreated(
-      //   EMPLOYEE_LISTENING_TEST_DATA.AWARENESS_CHECK.UPDATED_QUESTIONS.SINGLE.question
-      // );
+      await awarenessCheckPage.verifyAwarenessCheckQuestionIsCreated(
+        EMPLOYEE_LISTENING_TEST_DATA.AWARENESS_CHECK.UPDATED_QUESTIONS.SINGLE.question
+      );
     }
   );
 
@@ -211,9 +211,9 @@ test.describe('must Read and Awareness Check Content Functionality', () => {
 
       await awarenessCheckPage.removeAwarenessCheck();
 
-      // await awarenessCheckPage.verifyAwarenessCheckQuestionIsRemoved(
-      //   EMPLOYEE_LISTENING_TEST_DATA.AWARENESS_CHECK.QUESTIONS.SINGLE.question
-      // );
+      await awarenessCheckPage.verifyAwarenessCheckQuestionIsRemoved(
+        EMPLOYEE_LISTENING_TEST_DATA.AWARENESS_CHECK.QUESTIONS.SINGLE.question
+      );
     }
   );
 });
