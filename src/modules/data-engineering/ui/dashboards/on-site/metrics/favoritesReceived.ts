1ca15716b2961bb64f4a71c2cdd88250ccb5f0b4
Greyhat1991 <bharat.madaan@simpplr.com>
2026-02-03 17:20:23 +0530

    fix: remove Interaction Receiver User Code from CSV validation

--- src/modules/data-engineering/ui/dashboards/on-site/metrics/favoritesReceived.ts
+++ src/modules/data-engineering/ui/dashboards/on-site/metrics/favoritesReceived.ts
@@ -79,7 +79,6 @@ export class FavoritesReceived extends BaseOnSiteTabularMetricsComponent {
 
       const expectedCsvHeaders = [
         'Name',
-        'Interaction Receiver User Code',
         'Audience role',
         'Site name',
         'Site role',
@@ -103,7 +102,6 @@ export class FavoritesReceived extends BaseOnSiteTabularMetricsComponent {
         transformations: {
           headerMapping: {
             Name: 'Full name',
-            'Interaction Receiver User Code': 'Interacted by user code',
             'Audience role': 'Audience role',
             'Site name': 'Site name',
             'Site role': 'Site role',
